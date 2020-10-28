2e775897e71fe2777a408ba02035c68b769d68db
ramazan-dtas <60772908+ramazan-dtas@users.noreply.github.com>
2020-10-28 10:48:08 +0100

    v

---
+++ Atm/Atm/Class/BankAccount.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atm.Class
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance 
+        { get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransaction)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            } 
+        }
+        private static int accountNumberSeed = 1234567890;
+
+        private List<Transaction> allTransaction = new List<Transaction>();
+
+        public BankAccount(string name, decimal number)
+        {
+            Owner = name;
+            //Balance = number;
+            MakeDeposit(number, DateTime.Now, "Initial Balance");
+            Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransaction.Add(deposit);
+        }
+
+        public void MakeWithdrawl(decimal amount, DateTime date, string note)
+        {
+            if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if(Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransaction.Add(withdrawal);
+        }
+    }
+}
---
+++ Atm/Atm/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new Class.BankAccount("Kendra", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+
+            account.MakeWithdrawl(120, DateTime.Now, "Hammock");
+            Console.WriteLine(account.Balance);
+
+
+            try
+            {
+                var invalidAccount = new Class.BankAccount("invaid", -55);
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+            account.MakeWithdrawl(1200, DateTime.Now, "Lort");
+            Console.WriteLine(account.Balance);
+            Console.ReadKey();
+        }
+    }
+}
